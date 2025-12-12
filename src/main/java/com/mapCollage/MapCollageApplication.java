1d1369aa6f283bc9349bed85b63a466338b79e49
virendrayadav <virendrayadavv59@gmail.com>
2025-12-12 17:41:33 +0530

    FirstCommit

---
+++ src/main/java/com/mapCollage/MapCollageApplication.java
@@ -0,0 +1,14 @@
+package com.mapCollage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapCollageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapCollageApplication.class, args);
+		System.out.println("Run successFully");
+	}
+
+}
---
+++ src/test/java/com/mapCollage/MapCollageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapCollage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapCollageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
